5f26bdfbd289ed01e2721faaba30b198f3205aa1
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-09-03 13:36:34 +0700

    form completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,32 @@ import React, { useState } from "react";
 import Logo from "./Logo";
 
 function Home() {
-  const [isLolos, setIsLolos] = useState(true);
+  // Data Camaba
+  const [noReg, setNoReg] = useState("");
+  const [nama, setNama] = useState("");
+  const [lahir, setLahir] = useState({});
 
-  const handleSetlolos = (param) => setIsLolos(() => param);
+  const [snm, setSnm] = useState(true);
+  const [sekolah, setSekolah] = useState("");
+  const [domisili, setDomisili] = useState({});
+
+  const [isLolos, setIsLolos] = useState(true);
+  const [univ, setUniv] = useState("");
+  const [prodi, setProdi] = useState("");
+
+  // Seluruh data camaba
+  const [data, setData] = useState({});
+
+  const handleSetParam = (param, setParam) => setParam(() => param);
+
+  const inputDataHandler = (e, setInput) => {
+    setInput(() => e.target.value);
+  };
+
+  const submitDataHandler = (e) => {
+    e.preventDefault();
+    return console.log("tes");
+  };
 
   return (
     <div>
@@ -17,7 +40,22 @@ function Home() {
           Silakan isi formulir di bawah.
         </p>
       </div>
-      <form action="">
+
+      {/* Form Register Camaba */}
+      <form onSubmit={submitDataHandler} action="">
+        <div className="flex flex-col mb-8">
+          <label className="mb-2 font-bold text-black text-opacity-30">
+            Nomor Peserta
+          </label>
+          <input
+            onChange={(e) => inputDataHandler(e, setNoReg)}
+            value={noReg}
+            className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
+            type="text"
+            placeholder="Nomor Peserta"
+            required
+          />
+        </div>
         <div className="flex flex-col mb-8">
           <label className="mb-2 font-bold text-black text-opacity-30">
             Nama Peserta
@@ -60,24 +98,24 @@ function Home() {
           </label>
           <div className="flex text-center">
             <div
-              onClick={() => handleSetlolos(true)}
+              onClick={() => handleSetParam(true, setSnm)}
               className={`
                 w-full rounded-md shadow
                 p-3 sm:p-4 mr-4
                 font-bold text-black text-opacity-60 
-                ${isLolos ? "bg-green-300" : "bg-white"} 
+                ${snm ? "bg-gray-300" : "bg-white"} 
                 cursor-pointer
               `}
             >
               SNMPTN
             </div>
             <div
-              onClick={() => handleSetlolos(false)}
+              onClick={() => handleSetParam(false, setSnm)}
               className={`
                 p-3 sm:p-4 mr-4
                 w-full rounded-md shadow
                 font-bold text-black text-opacity-60 
-                ${!isLolos ? "bg-red-300" : "bg-white"} 
+                ${!snm ? "bg-gray-300" : "bg-white"} 
                 cursor-pointer
               `}
             >
@@ -85,31 +123,67 @@ function Home() {
             </div>
           </div>
         </div>
+        {snm && (
+          <>
+            <div className="flex flex-col mb-8">
+              <label className="mb-2 font-bold text-black text-opacity-30">
+                Asal Sekolah
+              </label>
+              <input
+                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
+                type="text"
+                placeholder="Asal Sekolah"
+                required
+              />
+            </div>
+            <div className="flex flex-col mb-8">
+              <label className="mb-2 font-bold text-black text-opacity-30">
+                Domisili
+              </label>
+              <div className="flex text-sm sm:text-base">
+                <input
+                  className="w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none"
+                  type="text"
+                  placeholder="Provinsi"
+                  required
+                />
+                <input
+                  className="w-full p-3 sm:p-4 rounded-md shadow focus:outline-none"
+                  type="text"
+                  placeholder="Kab/Kota"
+                  required
+                />
+              </div>
+            </div>
+          </>
+        )}
         <div className="flex flex-col mb-8">
           <label className="mb-2 font-bold text-black text-opacity-30">
             Hasil
           </label>
           <div className="flex text-center">
             <div
-              onClick={() => handleSetlolos(true)}
+              onClick={() => handleSetParam(true, setIsLolos)}
               className={`
                 w-full rounded-md shadow
                 p-3 sm:p-4 mr-4
                 font-bold text-black text-opacity-60 
-                ${isLolos ? "bg-green-300" : "bg-white"} 
+                ${isLolos ? "bg-green-300" : "bg-white hover:bg-green-100"} 
                 cursor-pointer
+                duration-200
               `}
             >
               Lolos
             </div>
             <div
-              onClick={() => handleSetlolos(false)}
+              onClick={() => handleSetParam(false, setIsLolos)}
               className={`
                 p-3 sm:p-4 mr-4
                 w-full rounded-md shadow
                 font-bold text-black text-opacity-60 
-                ${!isLolos ? "bg-red-300" : "bg-white"} 
+                ${!isLolos ? "bg-red-300" : "bg-white hover:bg-red-100"} 
                 cursor-pointer
+                duration-200
               `}
             >
               Gagal
@@ -117,48 +191,43 @@ function Home() {
           </div>
         </div>
         {isLolos && (
-          <div className="flex flex-col mb-8">
-            <label className="mb-2 font-bold text-black text-opacity-30">
-              Universitas
-            </label>
-            <input
-              className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-              type="text"
-              placeholder="Universitas Tujuan"
-              required
-            />
-          </div>
+          <>
+            <div className="flex flex-col mb-8">
+              <label className="mb-2 font-bold text-black text-opacity-30">
+                Universitas
+              </label>
+              <input
+                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
+                type="text"
+                placeholder="Universitas Tujuan"
+                required
+              />
+            </div>
+            <div className="flex flex-col mb-8">
+              <label className="mb-2 font-bold text-black text-opacity-30">
+                Program Studi
+              </label>
+              <input
+                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
+                type="text"
+                placeholder="Program Studi"
+                required
+              />
+            </div>
+          </>
         )}
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
-            Asal Sekolah
-          </label>
-          <input
-            className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-            type="text"
-            placeholder="Asal Sekolah"
-            required
-          />
-        </div>
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
-            Domisili
-          </label>
-          <div className="flex text-sm sm:text-base">
-            <input
-              className="w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none"
-              type="text"
-              placeholder="Provinsi"
-              required
-            />
-            <input
-              className="w-full p-3 sm:p-4 rounded-md shadow focus:outline-none"
-              type="text"
-              placeholder="Kab/Kota"
-              required
-            />
-          </div>
-        </div>
+        <button
+          type="submit"
+          className="
+          w-full bg-gray-300 p-3 sm:p-4 
+          rounded-md shadow 
+          text-sm sm:text-base font-bold
+          hover:bg-gray-200
+          duration-200
+        "
+        >
+          Generate
+        </button>
       </form>
     </div>
   );
