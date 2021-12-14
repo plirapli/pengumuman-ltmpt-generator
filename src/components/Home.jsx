0ebd8f673521790eb14962d1124621f399b25543
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-12-14 22:30:28 +0700

    add close modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,11 @@ import { useHistory } from 'react-router';
 import Logo from './Logo';
 import logoLtmpt from '../assets/images/logo-ltmpt.png';
 
-function Home({ dataMaba, setDataMaba }) {
+function Home({ setDataMaba }) {
   let history = useHistory();
+
+  // Button
+  const [click, setClick] = useState(true);
 
   // Data Domisili
   const [provinsi, setProvinsi] = useState([]);
@@ -123,91 +126,98 @@ function Home({ dataMaba, setDataMaba }) {
   return (
     <div>
       <Logo logo={logoLtmpt} />
-      <div className="mb-4">
-        <h1 className="mt-4 text-3xl font-bold uppercase">
+      <div className='mb-4'>
+        <h1 className='mt-4 text-3xl font-bold uppercase'>
           {`hasil seleksi ${snm ? 'snmptn' : 'sbmptn'} 2022`}
         </h1>
-        <p className="text-black text-opacity-30">
+        <p className='text-black text-opacity-30'>
           Silakan isi formulir di bawah ini.
         </p>
       </div>
 
-      <div className="bg-blue-200 my-4 p-3 sm:p-4 rounded-md shadow text-sm text-black text-opacity-30 uppercase">
+      <div
+        onClick={() => setClick(() => !click)}
+        className={`${
+          click ? 'flex' : 'hidden'
+        } justify-between bg-blue-200 my-4 p-3 sm:p-4 rounded-md shadow text-sm text-black text-opacity-30 uppercase`}>
         Data yang telah anda isi tidak akan dikirimkan ke server mana pun.
+        <span className='cursor-pointer hover:text-black transition-all'>
+          &#10005;
+        </span>
       </div>
 
       {/* Form Register Camaba */}
-      <form onSubmit={submitDataHandler} action="">
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
+      <form onSubmit={submitDataHandler} action=''>
+        <div className='flex flex-col mb-8'>
+          <label className='mb-2 font-bold text-black text-opacity-30'>
             Nomor Peserta
           </label>
           <input
             onChange={(e) => inputDataHandler(e, setNoReg, true)}
             value={noReg}
-            className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-            type="name"
-            placeholder="No. Peserta"
+            className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+            type='name'
+            placeholder='No. Peserta'
             maxLength={snm ? '10' : '12'}
             required
           />
         </div>
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
+        <div className='flex flex-col mb-8'>
+          <label className='mb-2 font-bold text-black text-opacity-30'>
             Nama Peserta
           </label>
           <input
             onChange={(e) => inputDataHandler(e, setNama)}
             value={nama}
-            className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-            type="text"
-            placeholder="Nama Peserta"
+            className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+            type='text'
+            placeholder='Nama Peserta'
             required
           />
         </div>
 
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
+        <div className='flex flex-col mb-8'>
+          <label className='mb-2 font-bold text-black text-opacity-30'>
             Tanggal Lahir
           </label>
-          <div className="flex text-sm sm:text-base">
+          <div className='flex text-sm sm:text-base'>
             <input
               onChange={(e) => inputDataObjHandler(e, setLahir, true)}
-              name="d"
+              name='d'
               value={lahir.d}
-              className="w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none text-center"
-              type="text"
-              placeholder="Tanggal"
-              maxLength="2"
+              className='w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none text-center'
+              type='text'
+              placeholder='Tanggal'
+              maxLength='2'
               required
             />
             <input
               onChange={(e) => inputDataObjHandler(e, setLahir, true)}
-              name="m"
+              name='m'
               value={lahir.m}
-              className="w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none text-center"
-              type="text"
-              placeholder="Bulan"
-              maxLength="2"
+              className='w-full p-3 sm:p-4 mr-4 rounded-md shadow focus:outline-none text-center'
+              type='text'
+              placeholder='Bulan'
+              maxLength='2'
               required
             />
             <input
               onChange={(e) => inputDataObjHandler(e, setLahir, true)}
-              name="y"
+              name='y'
               value={lahir.y}
-              className="w-full p-3 sm:p-4 rounded-md shadow focus:outline-none text-center"
-              type="text"
-              placeholder="Tahun"
-              maxLength="4"
+              className='w-full p-3 sm:p-4 rounded-md shadow focus:outline-none text-center'
+              type='text'
+              placeholder='Tahun'
+              maxLength='4'
               required
             />
           </div>
         </div>
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
+        <div className='flex flex-col mb-8'>
+          <label className='mb-2 font-bold text-black text-opacity-30'>
             Jenis Seleksi
           </label>
-          <div className="flex text-center">
+          <div className='flex text-center'>
             <div
               onClick={() => handleSetParam(true, setSnm)}
               className={`
@@ -217,8 +227,7 @@ function Home({ dataMaba, setDataMaba }) {
                 ${snm ? 'bg-gray-300' : 'bg-white hover:bg-gray-200'} 
                 cursor-pointer
                 duration-200
-              `}
-            >
+              `}>
               SNMPTN
             </div>
             <div
@@ -230,93 +239,89 @@ function Home({ dataMaba, setDataMaba }) {
                 ${!snm ? 'bg-gray-300' : 'bg-white hover:bg-gray-200'} 
                 cursor-pointer
                 duration-200
-              `}
-            >
+              `}>
               SBMPTN
             </div>
           </div>
         </div>
         {snm && (
           <>
-            <div className="flex flex-col mb-8">
-              <label className="mb-2 font-bold text-black text-opacity-30">
+            <div className='flex flex-col mb-8'>
+              <label className='mb-2 font-bold text-black text-opacity-30'>
                 NISN
               </label>
               <input
                 onChange={(e) => inputDataHandler(e, setNisn, true)}
                 value={nisn}
-                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-                type="text"
-                placeholder="NISN"
-                maxLength="10"
+                className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+                type='text'
+                placeholder='NISN'
+                maxLength='10'
                 required
               />
             </div>
-            <div className="flex flex-col mb-8">
-              <label className="mb-2 font-bold text-black text-opacity-30">
+            <div className='flex flex-col mb-8'>
+              <label className='mb-2 font-bold text-black text-opacity-30'>
                 Asal Sekolah
               </label>
               <input
                 onChange={(e) => inputDataHandler(e, setSekolah)}
                 value={sekolah}
-                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-                type="text"
-                placeholder="Asal Sekolah"
+                className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+                type='text'
+                placeholder='Asal Sekolah'
                 required
               />
             </div>
-            <div className="flex flex-col mb-8">
-              <label className="mb-2 font-bold text-black text-opacity-30">
+            <div className='flex flex-col mb-8'>
+              <label className='mb-2 font-bold text-black text-opacity-30'>
                 Domisili
               </label>
-              <div className="flex text-sm sm:text-base">
-                <div className="w-full flex items-center relative">
+              <div className='flex text-sm sm:text-base'>
+                <div className='w-full flex items-center relative'>
                   <select
-                    name="prov"
-                    title="Daftar Provinsi"
-                    className="w-full p-3 pr-8 sm:p-4 sm:pr-10 mr-4 rounded-md shadow focus:outline-none appearance-none overflow-ellipsis"
+                    name='prov'
+                    title='Daftar Provinsi'
+                    className='w-full p-3 pr-8 sm:p-4 sm:pr-10 mr-4 rounded-md shadow focus:outline-none appearance-none overflow-ellipsis'
                     onChange={(e) => {
                       inputDataObjHandler(e, setDomisili, false, true);
-                    }}
-                  >
+                    }}>
                     {provinsi.map((prov) => (
                       <option
                         data-prov={prov.id}
                         key={prov.id}
-                        value={prov.name}
-                      >
+                        value={prov.name}>
                         {prov.name}
                       </option>
                     ))}
                   </select>
-                  <div className="absolute right-7">▼</div>
+                  <div className='absolute right-7'>▼</div>
                 </div>
 
-                <div className="w-full flex items-center relative ">
+                <div className='w-full flex items-center relative '>
                   <select
-                    name="kotaKab"
-                    title="Daftar Kabupaten"
-                    id=""
-                    className="w-full p-3 pr-8 sm:p-4 sm:pr-12 rounded-md shadow focus:outline-none appearance-none overflow-ellipsis"
-                    onChange={(e) => inputDataObjHandler(e, setDomisili)}
-                  >
+                    name='kotaKab'
+                    title='Daftar Kabupaten'
+                    id=''
+                    className='w-full p-3 pr-8 sm:p-4 sm:pr-12 rounded-md shadow focus:outline-none appearance-none overflow-ellipsis'
+                    onChange={(e) => inputDataObjHandler(e, setDomisili)}>
                     {kotaKab.map((regency) => (
                       <option key={regency.id} value={regency.name}>
                         {regency.name}
                       </option>
                     ))}
                   </select>
-                  <div className="absolute right-2">▼</div>
+                  <div className='absolute right-2'>▼</div>
                 </div>
               </div>
             </div>
           </>
         )}
-        <div className="flex flex-col mb-8">
-          <label className="mb-2 font-bold text-black text-opacity-30">
+        <div className='flex flex-col mb-8'>
+          <label className='mb-2 font-bold text-black text-opacity-30'>
             Hasil
           </label>
-          <div className="flex text-center">
+          <div className='flex text-center'>
             <div
               onClick={() => handleSetParam(true, setIsLulus)}
               className={`
@@ -326,8 +331,7 @@ function Home({ dataMaba, setDataMaba }) {
                 ${isLulus ? 'bg-green-300' : 'bg-white hover:bg-green-100'} 
                 cursor-pointer
                 duration-200
-              `}
-            >
+              `}>
               Lulus
             </div>
             <div
@@ -339,55 +343,54 @@ function Home({ dataMaba, setDataMaba }) {
                 ${!isLulus ? 'bg-red-300' : 'bg-white hover:bg-red-100'} 
                 cursor-pointer
                 duration-200
-              `}
-            >
+              `}>
               Gagal
             </div>
           </div>
         </div>
         {isLulus && (
           <>
-            <div className="flex flex-col mb-8">
-              <label className="mb-2 font-bold text-black text-opacity-30">
+            <div className='flex flex-col mb-8'>
+              <label className='mb-2 font-bold text-black text-opacity-30'>
                 Universitas
               </label>
               <input
                 onChange={(e) => inputDataHandler(e, setUniv)}
                 value={univ}
-                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-                type="text"
-                placeholder="Universitas Tujuan"
+                className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+                type='text'
+                placeholder='Universitas Tujuan'
                 required
               />
             </div>
-            <div className="flex flex-col mb-8">
-              <label className="mb-2 font-bold text-black text-opacity-30">
+            <div className='flex flex-col mb-8'>
+              <label className='mb-2 font-bold text-black text-opacity-30'>
                 Program Studi
               </label>
               <input
                 onChange={(e) => inputDataHandler(e, setProdi)}
                 value={prodi}
-                className="p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base"
-                type="text"
-                placeholder="Program Studi"
+                className='p-3 sm:p-4 rounded-md shadow focus:outline-none text-sm sm:text-base'
+                type='text'
+                placeholder='Program Studi'
                 required
               />
             </div>
           </>
         )}
         <button
-          type="submit"
-          className="
+          type='submit'
+          className='
           w-full bg-gray-300 p-3 sm:p-4 
           rounded-md shadow 
           text-sm sm:text-base font-bold
           hover:bg-gray-200
           duration-200
-        "
-        >
+        '>
           Generate
         </button>
       </form>
+      {/* End Form Register Camaba */}
     </div>
   );
 }
