a13def3b165d76ed643ef30ebc85f8be1ce08166
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-02-13 03:26:25 +0700

    dynamic siE

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -1,11 +1,11 @@
-import qrCode from "../../src/assets/images/qrcode.png";
+import qrCode from '../../src/assets/images/qrcode.png';
 
-function QrCode() {
+function QrCode(props) {
   return (
     <img
-      className="w-32 h-32 mb-4 sm:mb-0 sm:mr-4 rounded-md shadow"
+      className={`${props.size} mb-4 sm:mb-0 sm:mr-4 rounded-md shadow`}
       src={qrCode}
-      alt=""
+      alt=''
     />
   );
 }
