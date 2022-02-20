80e82a0492234c8b63c90904f5358d87d3c69bbc
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-02-20 19:38:03 +0700

    fix grid bug

--- src/components/SbmOriginal.jsx
+++ src/components/SbmOriginal.jsx
@@ -1,3 +1,4 @@
+import ProfileLayout from './sbm/theme0/ProfileLayout';
 import Logo from './Logo';
 import logoLtmpt from '../assets/images/logo-ltmpt.png';
 import logoSponsor from '../assets/images/sponsor.png';
@@ -7,9 +8,6 @@ import { Link } from 'react-router-dom';
 function Snm({ dataMaba }) {
   const kalimatGagal =
     'semangat dan jangan putus asa! masih ada kesempatan mengikuti seleksi mandiri ptn.';
-
-  const kalimatDaful =
-    'Informasi pendaftaran ulang di PTN/Politeknik Negeri dapat dilihat pada link berikut.';
 
   return (
     <div className='w-full'>
@@ -23,7 +21,7 @@ function Snm({ dataMaba }) {
           </div>
         </div>
 
-        <div className='w-full h-8 bg-blue-500'></div>
+        <div className='w-full h-8 bg-blue-700'></div>
 
         {/* Body */}
         <div className='w-full max-w-screen-md lg:max-w-screen-lg'>
@@ -40,70 +38,54 @@ function Snm({ dataMaba }) {
               )}
               <div className='p-6 sm:ml-6 bg-gray-200 sm:col-span-8'>
                 <div className='flex flex-col'>
-                  <div className='flex'>
-                    <p className='w-32 border border-red-500'>Nomor peserta</p>
-                    <span className='mx-4 border border-red-500'>:</span>
-                    <p className='flex-grow border border-red-500 font-bold uppercase'>
-                      {dataMaba.noReg || '123-123-12-0123'}
-                    </p>
+                  {/* Profile */}
+                  <div>
+                    <ProfileLayout
+                      field={'Nomer Peserta'}
+                      profile={dataMaba.noReg || '123-123-12-0123'}
+                    />
+                    <ProfileLayout
+                      field={'Nama'}
+                      profile={dataMaba.nama || 'John Doe'}
+                    />
+                    <ProfileLayout
+                      field={'Tanggal Lahir'}
+                      profile={dataMaba.tglLahir || '05-04-2003'}
+                    />
                   </div>
-                  <div className='flex'>
-                    <p className='w-32 border border-red-500'>Nama</p>
-                    <span className='mx-4 border border-red-500'>:</span>
-                    <p className='flex-grow border border-red-500 font-bold uppercase'>
-                      {dataMaba.nama || 'John Doe'}
-                    </p>
-                  </div>
-                  <div className='flex'>
-                    <p className='w-32 border border-red-500'>Tanggal lahir</p>
-                    <span className='mx-4 border border-red-500'>:</span>
-                    <p className='flex-grow border border-red-500 font-bold uppercase'>
-                      {dataMaba.tglLahir || '31-12-2012'}
-                    </p>
-                  </div>
+
+                  {/* PTN */}
                   <div className='mt-6'>
                     <p>
                       Selamat! Anda dinyatakan lulus seleksi SBMPTN LTMPT 2021
                       di:
                     </p>
-                    <div className='flex'>
-                      <p className='w-32 border border-red-500'>PTN</p>
-                      <span className='mx-4 border border-red-500'>:</span>
-                      <p className='flex-grow border border-red-500 font-bold uppercase'>
-                        {dataMaba.univ || 'Institut Neo Kultur'}
-                      </p>
-                    </div>
-                    <div className='flex'>
-                      <p className='w-32 border border-red-500'>
-                        Program Studi
-                      </p>
-                      <span className='mx-4 border border-red-500'>:</span>
-                      <p className='flex-grow border border-red-500 font-bold uppercase'>
-                        {dataMaba.prodi || 'Informatika'}
-                      </p>
-                    </div>
+                    <ProfileLayout
+                      field={'PTN'}
+                      profile={dataMaba.univ || 'Institut Neo Kultur'}
+                    />
+                    <ProfileLayout
+                      field={'Program Studi'}
+                      profile={dataMaba.prodi || 'Informatika'}
+                    />
                   </div>
+                  <p className='mt-6'>
+                    Persyaratan pendaftaran ulang calon mahasiswa baru dapat
+                    dilihat di sini.
+                  </p>
+                  <p className='mt-6'>
+                    Anda dapat mencetak kembali Kartu Tanda Peserta UTBK-SBMPTN
+                    2021 di sini.
+                  </p>
+                  <button className='mt-6 p-2 rounded bg-teal-400 bg-blue-500 text-white animate-pulse'>
+                    UNDUH PENGUMUMAN KETUA LTMPT (PDF)
+                  </button>
                 </div>
-
-                {/* {dataMaba.isLulus && (
-                  <div className='mt-2 flex flex-col sm:text-lg uppercase'>
-                    <h3 className='font-bold'>
-                      {dataMaba.univ || 'Institut Neo Kultur'}
-                    </h3>
-                    <h3>
-                      {' '}
-                      Program Studi{' '}
-                      <span className='font-bold'>
-                        {dataMaba.prodi || 'informatika'}
-                      </span>
-                    </h3>
-                  </div>
-                )} */}
               </div>
             </div>
 
             {/* Tombol kembali */}
-            <div className='bg-gray-200 p-4 mt-8'>
+            <div className='bg-gray-200 p-4'>
               <Link to='/'>
                 <button className='w-full p-2 rounded bg-green-500 text-white'>
                   Kembali ke pencarian
