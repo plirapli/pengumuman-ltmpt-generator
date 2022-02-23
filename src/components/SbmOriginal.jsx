265d2b0075b7b1b17cd61f30268715bf43efc1cc
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-02-23 21:17:14 +0700

    add gagal ui

--- src/components/SbmOriginal.jsx
+++ src/components/SbmOriginal.jsx
@@ -6,11 +6,9 @@ import QrCode from './QrCode';
 import { Link } from 'react-router-dom';
 
 function Snm({ dataMaba }) {
-  const kalimatGagal =
-    'semangat dan jangan putus asa! masih ada kesempatan mengikuti seleksi mandiri ptn.';
-
   return (
     <div className='w-full flex flex-col items-center'>
+      {/* Header */}
       <div className='p-4 grid grid-cols-12 place-content-center'>
         <div className='flex justify-center col-span-12 sm:col-span-5'>
           <Link to='/'>
@@ -27,62 +25,82 @@ function Snm({ dataMaba }) {
       {/* Body */}
       <div className='w-full max-w-screen-md lg:max-w-screen-lg'>
         <div className='my-8 text-xl font-bold text-center'>
-          PENGUMUMAN HASIL SELEKSI SBMPTN LTMPT 2021
+          PENGUMUMAN HASIL SELEKSI SBMPTN LTMPT 2022
         </div>
 
         {/* Hasil */}
         <div className='border border-gray-300 rounded-md overflow-hidden'>
-          {/* Profil Siswa */}
-          <div className='p-6 grid sm:grid-cols-12'>
-            {dataMaba.isLulus && (
-              <QrCode size='sm:col-span-4 w-11/12 sm:w-full' />
-            )}
-            <div className='bg-gray-200 p-6 mt-2 sm:mt-0 sm:ml-6 sm:col-span-8'>
-              <div className='flex flex-col'>
-                {/* Profile */}
-                <div>
-                  <ProfileLayout
-                    field={'Nomer Peserta'}
-                    profile={dataMaba.noReg || '123-123-12-0123'}
-                  />
-                  <ProfileLayout
-                    field={'Nama'}
-                    profile={dataMaba.nama || 'John Doe'}
-                  />
-                  <ProfileLayout
-                    field={'Tanggal Lahir'}
-                    profile={dataMaba.tglLahir || '05-04-2003'}
-                  />
-                </div>
+          <div className='p-6 text-center'>
+            <p className='uppercase'>
+              mohon maaf, peserta atas nama
+              <span className='font-bold'> {dataMaba.nama || `John Doe`} </span>
+              dengan nomor peserta
+              <span className='font-bold'>
+                {' '}
+                {dataMaba.noReg || `123-123-12-0123`}{' '}
+              </span>
+              dinyatakan <span className='font-bold'>tidak diterima </span>
+              pada sbmptn ltmpt 2022
+            </p>
+            <p className='mt-4 text-xl font-bold'>
+              JANGAN PUTUS ASA DAN TETAP SEMANGAT!
+            </p>
+          </div>
 
-                {/* PTN */}
-                <div className='mt-6'>
-                  <p>
-                    Selamat! Anda dinyatakan lulus seleksi SBMPTN LTMPT 2021 di:
+          {/* Profil Siswa */}
+          {dataMaba.isLulus && (
+            <div className='p-6 grid sm:grid-cols-12'>
+              {dataMaba.isLulus && (
+                <QrCode size='sm:col-span-4 w-11/12 sm:w-full' />
+              )}
+              <div className='bg-gray-200 p-6 mt-2 sm:mt-0 sm:ml-6 sm:col-span-8'>
+                <div className='flex flex-col'>
+                  {/* Profile */}
+                  <div>
+                    <ProfileLayout
+                      field={'Nomer Peserta'}
+                      profile={dataMaba.noReg || '123-123-12-0123'}
+                    />
+                    <ProfileLayout
+                      field={'Nama'}
+                      profile={dataMaba.nama || 'John Doe'}
+                    />
+                    <ProfileLayout
+                      field={'Tanggal Lahir'}
+                      profile={dataMaba.tglLahir || '05-04-2003'}
+                    />
+                  </div>
+
+                  {/* PTN */}
+                  <div className='mt-6'>
+                    <p>
+                      Selamat! Anda dinyatakan lulus seleksi SBMPTN LTMPT 2021
+                      di:
+                    </p>
+                    <ProfileLayout
+                      field={'PTN'}
+                      profile={dataMaba.univ || 'Institut Neo Kultur'}
+                    />
+                    <ProfileLayout
+                      field={'Program Studi'}
+                      profile={dataMaba.prodi || 'Informatika'}
+                    />
+                  </div>
+                  <p className='mt-6'>
+                    Persyaratan pendaftaran ulang calon mahasiswa baru dapat
+                    dilihat di sini.
                   </p>
-                  <ProfileLayout
-                    field={'PTN'}
-                    profile={dataMaba.univ || 'Institut Neo Kultur'}
-                  />
-                  <ProfileLayout
-                    field={'Program Studi'}
-                    profile={dataMaba.prodi || 'Informatika'}
-                  />
+                  <p className='mt-6'>
+                    Anda dapat mencetak kembali Kartu Tanda Peserta UTBK-SBMPTN
+                    2021 di sini.
+                  </p>
+                  <button className='mt-6 p-2 rounded bg-teal-400 bg-blue-500 text-white animate-pulse'>
+                    UNDUH PENGUMUMAN KETUA LTMPT (PDF)
+                  </button>
                 </div>
-                <p className='mt-6'>
-                  Persyaratan pendaftaran ulang calon mahasiswa baru dapat
-                  dilihat di sini.
-                </p>
-                <p className='mt-6'>
-                  Anda dapat mencetak kembali Kartu Tanda Peserta UTBK-SBMPTN
-                  2021 di sini.
-                </p>
-                <button className='mt-6 p-2 rounded bg-teal-400 bg-blue-500 text-white animate-pulse'>
-                  UNDUH PENGUMUMAN KETUA LTMPT (PDF)
-                </button>
               </div>
             </div>
-          </div>
+          )}
 
           {/* Tombol kembali */}
           <div className='bg-gray-200 p-4'>
